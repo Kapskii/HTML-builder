008ef70c9aaa5bca506a952d89acc8a0a980954f
Kapskii <kapskij20@gmail.com>
2024-01-22 11:10:38 +0300

    feat: add 02-write-file solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+const filePath = path.join(__dirname, 'textOutput.txt');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function writeToFile(text) {
+  fs.appendFile(filePath, text + '\n', (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+rl.setPrompt('Enter text or (press Ctrl + C or type "exit" to quit): ');
+rl.prompt();
+
+rl.on('line', (input) => {
+  if (input.toLowerCase() === 'exit') {
+    console.log('Bye - Bye!');
+    rl.close();
+  } else {
+    writeToFile(input);
+    rl.prompt();
+  }
+});
