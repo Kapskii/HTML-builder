41528e33ed654b026323d427d113f1d94f4a401c
Kapskii <kapskij20@gmail.com>
2024-01-22 12:33:03 +0300

    feat: add 04-copy-directory solution

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+async function dublicate() {
+  const sourceFolder = path.join(__dirname, 'files');
+  const destinationFolder = path.join(__dirname, 'files-copy');
+
+  try {
+    await fs.mkdir(destinationFolder, { recursive: true });
+
+    const files = await fs.readdir(sourceFolder);
+
+    for (const file of files) {
+      const sourceFP = path.join(sourceFolder, file);
+      const destinationFP = path.join(destinationFolder, file);
+
+      await fs.dublicateFile(sourceFP, destinationFP);
+    }
+
+    console.log('Directory dublication.');
+  } catch (error) {
+    console.error(`Error dublication: ${error.message}`);
+  }
+}
+
+dublicate();
